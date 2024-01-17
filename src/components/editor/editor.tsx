8181cd2cc80b6c3068d0e7d376a10978b2e47f93
Rachid <rachid@weareblox.com>
2024-01-17 20:52:01 +0100

    Improve editor

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -8,6 +8,9 @@ const height = 10;
 
 const cells = Array.from({ length: height }, () => Array(width).fill(CellState.Empty));
 
+const normalBorder = "1px solid lightgray"; // #6995C2, 316BB6
+const helpBorder = "1px solid #cf7776"; // #fa7875, 0844AB
+
 const solution: Array<Position> = [];
 for (let row = 0; row < height; row++) {
   for (let col = 0; col < width; col++) {
@@ -17,15 +20,21 @@ for (let row = 0; row < height; row++) {
 
 type SquareProps = {
   value: CellState;
+  position: { row: number; col: number };
+
   onClick: () => void;
 };
 
-const Square: React.FC<SquareProps> = ({ value, onClick }) => (
+const Square: React.FC<SquareProps> = ({ value, position, onClick }) => (
   <button
     style={{
       width: "40px",
       height: "40px",
       backgroundColor: value === CellState.Filled ? "black" : "white",
+      borderRight: position.col > 0 && (position.col + 1) % 5 === 0 ? helpBorder : normalBorder,
+      borderLeft: position.col % 5 === 0 ? helpBorder : normalBorder,
+      borderBottom: position.row > 0 && (position.row + 1) % 5 === 0 ? helpBorder : normalBorder,
+      borderTop: position.row % 5 === 0 ? helpBorder : normalBorder,
     }}
     onClick={onClick}
   >
@@ -41,22 +50,19 @@ const Editor: React.FC = () => {
   };
 
   return (
-    <div>
+    <div style={{ padding: "10px" }}>
       <div style={{ display: "flex" }}>
         <div style={{ width: "40px" }}></div>
-        {editorState.getColumnHeaders().map((header, index) => (
-          <div key={index} style={{ width: "40px", textAlign: "center" }}>
-            {header}
-          </div>
-        ))}
       </div>
       {editorState.getCells().map((row, rowIndex) => (
         <div key={rowIndex} style={{ display: "flex" }}>
-          <div style={{ width: "40px", textAlign: "center" }}>
-            {editorState.getRowHeaders()[rowIndex]}
-          </div>
           {row.map((cell, colIndex) => (
-            <Square key={colIndex} value={cell} onClick={() => handleClick(rowIndex, colIndex)} />
+            <Square
+              position={{ row: rowIndex, col: colIndex }}
+              key={colIndex}
+              value={cell}
+              onClick={() => handleClick(rowIndex, colIndex)}
+            />
           ))}
         </div>
       ))}
